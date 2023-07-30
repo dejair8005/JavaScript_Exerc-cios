a34be82ed26bb895f95bfd019b2a4d41972f015a
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-07-30 14:09:38 -0300

    commit

---
+++ Aulas/aula13.js
@@ -0,0 +1,12 @@
+let num=5
+
+
+if (num > 10) { // essas chaves são obrigatorias a partir de 2 comandos dentro do bloco
+    console.log("Numeral maior que 10")
+} else if( num > 5 ){
+    console.log("Numeral está entre 6 e 10")
+} else{ 
+    console.log("Numeral menor ou igual a 5")
+}
+
+console.log("Fim do progama") 

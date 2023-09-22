31bd76a42f0cdb1f707ef468b33d48200e6a6381
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-09-21 23:54:42 -0300

    updates

--- POO/index.js
+++ POO/index.js
@@ -1,38 +1,48 @@
 class Cliente {
     nome;
-    CPF;
-    Agencia;
-    Saldo;
-    rg;
+    cpf;    
 }
 
+
+class ContaCorrente {
+    agencia;
+    // #saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor){
+        
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if (valor <= 0) {
+           return
+        }
+        this._saldo += valor;
+        
+    }
+}
+
+
+
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
-
-
 cliente1.nome = "Ricardo";
-cliente1.CPF = 11122233309;
-cliente1.Agencia = 1001;
-cliente1.Saldo = 0;
-cliente1.rg = 123456;
+cliente1.cpf = "11122233309";
 
-
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
-cliente2.CPF = 88822233309;
-cliente2.Agencia = 1001;
-cliente2.Saldo = 0;
+cliente2.cpf = "88822233309";
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.depositar(100)
+const valorSacado = contaCorrenteRicardo.sacar(55)
+console.log(valorSacado)
 
-cliente3.nome = "Joana";
-cliente3.CPF = 88822233309;
-cliente3.Agencia = 1001;
-cliente3.Saldo = 0;
+console.log(contaCorrenteRicardo);
 
-
-
-
-console.log(cliente1, cliente2)
+//console.log(cliente1);
+//console.log(cliente2);
